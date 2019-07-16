1db49ae052acfff43983fba73f39076371974c1f
Colby Allen <colbyallen012@gmail.com>
2019-07-16 12:30:16 -0600

    state hooked up for movies with redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import { showMovies } from './actions'
+import { connect } from 'react-redux';
 import NavBar from './components/NavBar/NavBar';
 import MovieContainer from './components/MovieContainer/MovieContainer';
 import { fetchMovies } from './api/apiCalls';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      recentMovies: []
-    }
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     recentMovies: []
+  //   }
+  // }
   
   async componentDidMount() { 
     await fetchMovies()
-      .then(recentMovies => this.setState({ recentMovies }))
+      .then(movies => this.props.showMovies(movies))
   }
 
   render() {
     return (
       <div className="App">
         {/* <NavBar /> */}
-        <MovieContainer recentMovies={this.state.recentMovies}/>
+        {/* <MovieContainer /> */}
       </div>
     );
   }
 }
 
-export default App;
+  const mapStateToProps = (state) => ({
+    movies: state.movies
+  })
+
+  const mapDispatchToProps = (dispatch) => ({
+    showMovies: (movies) => dispatch (showMovies(movies))
+  })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
