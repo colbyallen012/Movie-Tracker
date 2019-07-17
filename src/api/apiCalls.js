1970c110ec918194a63981ae86b2b29477d93dd4
Aidan McKay <aidanmckay2000@gmail.com>
2019-07-17 17:34:39 -0600

    added tests for asynchronous fetch calls
    Co-authored-by: Antonio Fry <antoniofry2000@gmail.com>
    Co-authored-by: Colby Allen <colbyallen012@gmail.com>

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -1,7 +1,7 @@
   // const url = 'http://localhost:3000'
 import { apiKey } from '../apiKey'
 
-  export const fetchMovies = (params) => {
+  export const fetchMovies = () => {
       return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}edd&language=en-US&page=1`)
         .then(res => res.json())
         .then(res => res.results)
