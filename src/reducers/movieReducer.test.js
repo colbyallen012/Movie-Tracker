832d632c369a58711ec7ab48cb769f0fbb110ff7
Colby Allen <colbyallen012@gmail.com>
2019-07-21 14:34:12 -0600

    movieReducer tests
    Co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>
    Co-authored-by: Antonio Fry <antoniofry2000@gmail.com>

---
+++ src/reducers/movieReducer.test.js
@@ -0,0 +1,50 @@
+import { movieReducer } from '../reducers/movieReducer'
+
+describe ('movieReducer', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+    const result = movieReducer(undefined, {})
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should return the state with the movies array', () => {
+    const expected = [{
+      vote_count: 411,
+      id: 420818,
+      video: false,
+      vote_average: 7,
+      title: "The Lion King",
+      popularity: 585.503,
+      poster_path: "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg",
+      original_language: "en",
+      original_title: "The Lion King",
+      backdrop_path: "/1TUg5pO1VZ4B0Q1amk3OlXvlpXV.jpg",
+      adult: false,
+      overview: "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
+      release_date: "2019-07-12"
+    }];
+
+    const action = {
+      type: 'SHOW_MOVIES',
+      movies: [{
+        vote_count: 411,
+        id: 420818,
+        video: false,
+        vote_average: 7,
+        title: "The Lion King",
+        popularity: 585.503,
+        poster_path: "/dzBtMocZuJbjLOXvrl4zGYigDzh.jpg",
+        original_language: "en",
+        original_title: "The Lion King",
+        backdrop_path: "/1TUg5pO1VZ4B0Q1amk3OlXvlpXV.jpg",
+        adult: false,
+        overview: "Simba idolises his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub's arrival. Scar, Mufasa's brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba's exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.",
+        release_date: "2019-07-12"
+      }]
+    }
+
+    const result = movieReducer([], action)
+    expect(result).toEqual(expected)
+  })
+})
