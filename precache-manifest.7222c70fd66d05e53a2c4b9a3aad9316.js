af99b724b37712258d134facdcc7d4959c801e5e
Colby Allen <colbyallen012@gmail.com>
2019-11-18 11:09:21 -0700

    Updates

---
+++ precache-manifest.7222c70fd66d05e53a2c4b9a3aad9316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00bafd7a6dc1f93ca2ed82a08395ef5",
+    "url": "/Movie-Tracker/index.html"
+  },
+  {
+    "revision": "3997c268a9ac3764815f",
+    "url": "/Movie-Tracker/static/css/main.b6d01ae2.chunk.css"
+  },
+  {
+    "revision": "7b081a5e8719777f275a",
+    "url": "/Movie-Tracker/static/js/2.6c43f2ff.chunk.js"
+  },
+  {
+    "revision": "3997c268a9ac3764815f",
+    "url": "/Movie-Tracker/static/js/main.f724f7e4.chunk.js"
+  },
+  {
+    "revision": "f804ddde2dd9dc8fdf2b",
+    "url": "/Movie-Tracker/static/js/runtime~main.51f8b371.js"
+  }
+]);
