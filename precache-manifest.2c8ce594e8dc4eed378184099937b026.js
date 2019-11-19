ce76f38c2b245eaddb0349220f773cf8a0efb3d8
Colby Allen <colbyallen012@gmail.com>
2019-11-19 11:21:26 -0700

    Updates

--- precache-manifest.2c8ce594e8dc4eed378184099937b026.js
+++ precache-manifest.2c8ce594e8dc4eed378184099937b026.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e00bafd7a6dc1f93ca2ed82a08395ef5",
+    "revision": "0aa9e7d514d2ad199df91fd3ccc9ea37",
     "url": "/Movie-Tracker/index.html"
   },
   {
-    "revision": "3997c268a9ac3764815f",
-    "url": "/Movie-Tracker/static/css/main.b6d01ae2.chunk.css"
+    "revision": "0da5a6814f76cb533203",
+    "url": "/Movie-Tracker/static/css/main.dfc1081c.chunk.css"
   },
   {
     "revision": "7b081a5e8719777f275a",
     "url": "/Movie-Tracker/static/js/2.6c43f2ff.chunk.js"
   },
   {
-    "revision": "3997c268a9ac3764815f",
-    "url": "/Movie-Tracker/static/js/main.f724f7e4.chunk.js"
+    "revision": "0da5a6814f76cb533203",
+    "url": "/Movie-Tracker/static/js/main.223b04ad.chunk.js"
   },
   {
     "revision": "f804ddde2dd9dc8fdf2b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Movie-Tracker/precache-manifest.7222c70fd66d05e53a2c4b9a3aad9316.js"
+  "/Movie-Tracker/precache-manifest.2c8ce594e8dc4eed378184099937b026.js"
 );
 
 self.addEventListener('message', (event) => {
